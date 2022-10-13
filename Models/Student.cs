7e3701ed30c7425dfd0b6cc251ca7d54e376e966
Narongrit kasembunyakon <narongrit.kase@kmutt.ac.th>
2022-10-14 06:14:47 +0700

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+public class Student: Personal{
+    private string grade;
+    private string school;
+    
+    public Student(string prefix, string name, string surename,
+    string age, string grade, string allergic, string school, string religion)
+    : base (prefix, name, surename, age, allergic, religion){
+        
+        this.grade = grade;
+        this.school = school;
+    }
+}
